b8878958cd7744f6e9e64bf4881a55c2f648aa3e
Aarjav Chauhan <aarjavchauhan439@gmail.com>
2019-10-09 09:28:36 -0600

    added functionality for drawing circles

--- sketch.js
+++ sketch.js
@@ -9,9 +9,7 @@ var partsOfSpeech = []
 function setup() {
 
   createCanvas(600,600)
-  background(50)
-  textSize(20)
-  noStroke()
+  background(255)
 
   for (var i = 0; i < result.length-1; i++) {
     var words = RiTa.tokenize(result[i])
@@ -20,4 +18,35 @@ function setup() {
       partsOfSpeech.push(RiTa.getPosTags(words[j]))
     }
   }
+}
+
+function draw() {
+  //
+  drawPoints()
+}
+
+function drawPoints()
+{
+
+  if(frameCount%100 == 0 && frameCount/100 < wordsArray.length){
+    var x = random(width)
+    var y = random(height)
+    drawFromPoint(x, y)
+  //  line(x, y, x+frameCount/25, y+frameCount/25);
+    //text(wordsArray[frameCount/100], random(width), random(height))
+  }
+}
+
+function drawFromPoint(x,y,length,pos){
+  push()
+  noStroke()
+  fill(0, 0, 0, 125)
+  circle(x, y, 20)
+  pop()
+}
+
+function darken(transparency)
+{
+  // if
+  // return
 }
