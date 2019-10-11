65a3d0649216fd645f0c8b9ec15ff9107fb96e70
Aarjav Chauhan <aarjavchauhan439@gmail.com>
2019-10-10 22:45:53 -0600

    created the point object, colorized and determine length of the points

---
+++ Point.js
@@ -0,0 +1,41 @@
+class Point {
+  constructor(pointX, pointY, word, pos) {
+    this.x = pointX
+    this.y = pointY
+    this.hasReachedOpacity = false
+    this.hasBeenDisplayed = false
+    this.word = word
+    this.pos = pos
+  }
+
+  display(){
+    //console.log(this.pos)
+    push()
+  //  console.log(this.pos);
+    let colorThis = color(0,0,0,transparencyCounter)
+    if (this.pos == "nn") {
+      colorThis = color(165, 184, 128, transparencyCounter)
+    } else if (this.pos == "nnp") {
+      colorThis = color(155, 78, 110, transparencyCounter)
+    } else if (this.pos == "nns") {
+      colorThis = color(94, 111, 122, transparencyCounter)
+    } else if (this.pos == ";") {
+      colorThis = color(104, 121, 21, transparencyCounter)
+    } else if (this.pos == ".") {
+      colorThis = color(170, 144, 44, transparencyCounter)
+    } 
+
+    noStroke()
+      fill(colorThis)
+      circle(this.x, this.y, 5)
+      stroke(colorThis)
+      line(this.x, this.y, this.x+this.word.length*3, this.y+this.word.length*3)
+      this.hasBeenDisplayed = true
+    pop()
+  }
+
+  color()
+  {
+
+  }
+}
--- sketch.js
+++ sketch.js
@@ -5,12 +5,22 @@ function preload() {
 
 var wordsArray = []
 var partsOfSpeech = []
+let points = []
+const rate = 25
+var distinctPartsOfSpeech = []
+
+var transparencyCounter = 0
+var opaque = false
 
 function setup() {
 
   createCanvas(600,600)
   background(255)
 
+  createRitaArrays()
+}
+
+function createRitaArrays(){
   for (var i = 0; i < result.length-1; i++) {
     var words = RiTa.tokenize(result[i])
     for (var j=0; j < words.length; j++) {
@@ -18,35 +28,72 @@ function setup() {
       partsOfSpeech.push(RiTa.getPosTags(words[j]))
     }
   }
+  countPOS(partsOfSpeech)
 }
 
 function draw() {
-  //
+
+  determinePointTransparency()
   drawPoints()
+}
+
+function determinePointTransparency(){
+  if(transparencyCounter < 255 && !opaque)
+  {
+    transparencyCounter++
+  }
+  else if (transparencyCounter == 255) {
+    opaque = true
+    transparencyCounter--
+  }
+  else if (transparencyCounter < 255 && transparencyCounter != 0 && opaque) {
+    transparencyCounter--
+  }
+  else if (transparencyCounter == 0 && opaque) {
+    transparencyCounter++
+    opaque = false
+  }
 }
 
 function drawPoints()
 {
+  if(points.length == 0){
+    makePoint(width/2, height/2, wordsArray[0], partsOfSpeech[0])
+    points[0].display()
+  }
+  if(frameCount%rate == 0 && frameCount/rate < wordsArray.length){
+    var x = Math.ceil(random(width) / 10) * 10
+    var y = Math.ceil(random(height) / 10) * 10
+    makePoint(x, y, wordsArray[frameCount/rate], partsOfSpeech[frameCount/rate])
+  //line(x, y, x+frameCount/25, y+frameCount/25);
+  //text(wordsArray[frameCount/100], random(width), random(height))
+  }
 
-  if(frameCount%100 == 0 && frameCount/100 < wordsArray.length){
-    var x = random(width)
-    var y = random(height)
-    drawFromPoint(x, y)
-  //  line(x, y, x+frameCount/25, y+frameCount/25);
-    //text(wordsArray[frameCount/100], random(width), random(height))
+  for (var i = points.length-1; i > 0; i--) {
+    if (!points[i].hasBeenDisplayed)
+     points[i].display()
+    //console.log(i);
   }
 }
 
-function drawFromPoint(x,y,length,pos){
-  push()
-  noStroke()
-  fill(0, 0, 0, 125)
-  circle(x, y, 20)
-  pop()
+function makePoint(x,y,word,pos){
+  points.push(new Point(x, y, word, pos[0]))
+//  console.log(pos);
+  // for (var i = 0; i < distinctPartsOfSpeech.length; i++) {
+  //   if(distinctPartsOfSpeech[i]
+  // }
+  if (distinctPartsOfSpeech.indexOf(pos[0]) == -1) {
+    distinctPartsOfSpeech.push(pos[0])
+  }
 }
 
-function darken(transparency)
+function countPOS(a)
 {
-  // if
-  // return
+  var result = { };
+for(var i = 0; i < a.length; ++i) {
+    if(!result[a[i]])
+        result[a[i]] = 0;
+    ++result[a[i]];
+}
+console.log(result);
 }
