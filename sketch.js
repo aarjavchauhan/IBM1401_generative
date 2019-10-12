a42d0ec0331ccfb3e6831972d0ae90f692da68b3
Aarjav Chauhan <aarjavchauhan439@gmail.com>
2019-10-11 18:41:21 -0600

    show artist, album and numbers

--- sketch.js
+++ sketch.js
@@ -6,11 +6,24 @@ function preload() {
 var wordsArray = []
 var partsOfSpeech = []
 let points = []
-const rate = 25
+const rate = 14
 var distinctPartsOfSpeech = []
 
 var transparencyCounter = 0
 var opaque = false
+var textTransparency = 100
+var artistString = "Jóhann Jóhannsson"
+var artistArray = []
+var textShown = 0
+var textCheck = []
+var artistShown = false
+var albumString = "ibm 1401, a user's manual"
+var albumArray = []
+var albumTransparency = 0
+var textX = 0
+var textY = 0
+var numberArray = [4, 5, 6, 7, 8, 9]
+var numberRepeatArray = []
 
 function setup() {
 
@@ -18,6 +31,7 @@ function setup() {
   background(255)
 
   createRitaArrays()
+  setTextInformation()
 }
 
 function createRitaArrays(){
@@ -35,6 +49,8 @@ function draw() {
 
   determinePointTransparency()
   drawPoints()
+  showArtist()
+  showNumbers()
 }
 
 function determinePointTransparency(){
@@ -67,13 +83,16 @@ function drawPoints()
     makePoint(x, y, wordsArray[frameCount/rate], partsOfSpeech[frameCount/rate])
   //line(x, y, x+frameCount/25, y+frameCount/25);
   //text(wordsArray[frameCount/100], random(width), random(height))
+    showNumbers()
   }
 
   for (var i = points.length-1; i > 0; i--) {
-    if (!points[i].hasBeenDisplayed)
+    if (!points[i].hasBeenDisplayed){
      points[i].display()
+   }
     //console.log(i);
   }
+
 }
 
 function makePoint(x,y,word,pos){
@@ -95,5 +114,62 @@ for(var i = 0; i < a.length; ++i) {
         result[a[i]] = 0;
     ++result[a[i]];
 }
-console.log(result);
+//console.log(result);
+}
+
+function showArtist(){
+  push()
+    var textToShow = artistArray[Math.round(points.length/10)]
+    textAlign(LEFT)
+    textSize(20)
+    textFont('Courier New')
+    textStyle(ITALIC)
+    fill(0,0,0, textTransparency)
+    for (var i = 0; i < textCheck.length; i++) {
+        if(textCheck[i] == 0 && Math.round(points.length/10) == i){
+          text(textToShow, textX+(i*10), textY)
+          textCheck[i] = 1
+          break
+      }
+    }
+    if(Math.round(points.length/10) == 16)
+    {
+      fill(0,0,0, albumTransparency)
+      textSize(10)
+      text(albumString, textX+20, textY+25)
+        if(albumTransparency<=10)
+          albumTransparency += 2
+    }
+  pop()
+}
+
+function setTextInformation()
+{
+  textX = random(width-160)
+  textY = random(height-50)
+  artistArray = artistString.split('')
+  textCheck.length = artistArray.length
+  textCheck.fill(0)
+  albumArray = albumString.split('')
+  for (var i = 0; i < numberArray.length; i++) {
+    numberRepeatArray[i] = Math.floor(random(15, 40))
+  }
+}
+
+function showNumbers()
+{
+  push()
+    textSize(10)
+    textStyle(NORMAL)
+    fill(0,0,0, random(50, 100))
+    var number = numberArray[Math.floor(Math.random()*numberArray.length)]
+    var repeatState = numberArray.indexOf(number)
+    if(numberRepeatArray[repeatState] > 0){
+        translate(width-5-(points.length*20), height-10-(repeatState*20))
+        rotate(PI)
+        text(number, 0, 0)
+        numberRepeatArray[repeatState] --
+        console.log( numberRepeatArray);
+    }
+  pop()
 }
